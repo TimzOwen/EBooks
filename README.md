a51225bcc3efe8b4149a91446667694bbaf99a80
TimzOwen <ktimothy@kabarak.ac.ke>
2020-07-07 20:54:29 +0300

    API class

---
+++ README.md
@@ -0,0 +1,14 @@
+## E-Books Library
+This is a simple android library that fetches the data from google books API and posts it to the 
+android app using **JSON** File format.
+
+## Topics covered
+ 1.0 Shared Preference
+ 2.0 Parcelable classes 
+ 3.0 Picasso Library
+ 4.0 Data Binding 
+ 5.0 Fetching Data with JSON 
+ 6.0 RecyclerView
+ 7.0 Android Permissions
+ 
+     
---
+++ app/src/main/java/com/owen/ebooks/ApiUtil.java
@@ -0,0 +1,71 @@
+package com.owen.ebooks;
+
+import android.util.Log;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpCookie;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Scanner;
+
+public class ApiUtil {
+
+    //create an empty constructor;
+    private ApiUtil(){};
+
+    //create Constant that wont change using final keyword
+    public static final String BASE_API_URL = "https://www.googleapis.com/books/v1/volumes";
+
+    //create a URL builder function
+    public static URL buildURL(String title){
+        String fullUrlbuilder = BASE_API_URL + "?=" + title;
+
+        URL url = null;
+        try
+        {
+            url = new URL(fullUrlbuilder);
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        return url;
+    }
+    //connect the URL to JSON
+    public static String getJson(URL url) throws IOException
+    {
+        //establish  HTTP connection and open url connection
+        HttpURLConnection connection = (HttpURLConnection)  url.openConnection();
+        try
+        {
+            //create a scanner object and inputStream for conversion
+            InputStream inputStream = connection.getInputStream();
+            Scanner scanner = new Scanner(inputStream);
+
+            //check if the scanner has data by creating a boolean for it.
+            boolean hasData = scanner.hasNext();
+
+            if (hasData)
+            {
+                return scanner.next();
+            }
+            else
+            {
+                return null;
+            }
+        }
+        catch (Exception e)
+        {
+            Log.d("Error:", e.toString());
+            return null;
+        }
+        finally {
+            //disconnect the connection
+            connection.disconnect();
+        }
+
+
+    }
+}
